ed3a28911bc01f042c5f15802ccd2e6acbb556ef
Joshua Cotton <jcotton42@outlook.com>
2016-01-22 15:53:52 -0500

    Signup retreival done (I think)

--- Ion10/EighthSignup.cs
+++ Ion10/EighthSignup.cs
@@ -15,9 +15,10 @@ namespace Ion10 {
         public int ActivityID { get; set; }
         public string ActivityTitle { get; set; }
         public DateTime Date { get; set; }
-        public char BlockName { get; set; }
+        public string BlockName { get; set; }
 
         public static async Task<Collection<EighthSignup>> GetAsync(DateTime fromDate, int count) {
+#if false
             var results = new Collection<JsonArray>();
             var page = 1;
             var retrieved = 0;
@@ -28,8 +29,34 @@ namespace Ion10 {
                 results.Add(json.GetNamedArray("results"));
                 page++;
             } while(retrieved < count);
-            var blocks = results.SelectMany(r => r).Select(v => v.GetObject()).Take(count);
-
+            var blocks = results
+                .SelectMany(r => r)
+                .Select(v => v.GetObject())
+                .OrderBy(b => b.GetNamedString("date"))
+                .ThenBy(b => b.GetNamedString("block_letter"))
+                .Take(count);
+#endif
+            fromDate = fromDate.Date;
+            var json = JsonArray
+                .Parse(await App.HttpClient.GetStringAsync(new Uri("/api/signups/user?format=json")))
+                .Select(o => o.GetObject())
+                .Where(s => DateTime.Parse(s.GetNamedObject("block").GetNamedString("date")) >= fromDate)
+                .Take(count);
+            var results = new Collection<EighthSignup>();
+            foreach(var signup in json) {
+                var blockid = (int)signup.GetNamedObject("block").GetNamedNumber("id");
+                var activity = signup.GetNamedObject("activity");
+                results.Add(new EighthSignup {
+                    ActivityID = (int)activity.GetNamedNumber("id"),
+                    ActivityTitle = activity.GetNamedString("title"),
+                    BlockID = blockid,
+                    BlockName =
+                        JsonObject.Parse(
+                            await App.HttpClient.GetStringAsync(new Uri($"/api/blocks/{blockid}?format=json")))
+                            .GetNamedString("block_letter")
+                });
+            }
+            return results;
         }
     }
 }
