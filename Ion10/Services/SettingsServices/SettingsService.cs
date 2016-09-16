2eba0842993de28090508e4da7dacb948c53315a
Joshua Cotton <jcotton42@outlook.com>
2016-09-16 17:32:15 -0400

    Add OAuth settings

--- Ion10/Services/SettingsServices/SettingsService.cs
+++ Ion10/Services/SettingsServices/SettingsService.cs
@@ -1,4 +1,10 @@
 using System;
+using System.Collections.Immutable;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Security.Credentials;
+using Windows.Storage;
 using Template10.Common;
 using Template10.Utils;
 using Windows.UI.Xaml;
@@ -7,8 +13,54 @@ namespace Ion10.Services.SettingsServices {
     public class SettingsService {
         public static SettingsService Instance { get; } = new SettingsService();
         Template10.Services.SettingsService.ISettingsHelper _helper;
+        private int initialized;
+        private Uri baseUri;
+        private string oauthId;
+        private string oauthSecret;
+        private Uri oauthCallbackUri;
+
         private SettingsService() {
             _helper = new Template10.Services.SettingsService.SettingsHelper();
+        }
+
+        public async Task InitializeAsync() {
+            if(Interlocked.Exchange(ref initialized, 1) == 1) {
+                return;
+            }
+            var configFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/config.txt"));
+            using(var stream = await configFile.OpenStreamForReadAsync())
+            using(var reader = new StreamReader(stream)) {
+                baseUri = new Uri(reader.ReadLine());
+                oauthId = reader.ReadLine();
+                oauthSecret = reader.ReadLine();
+                oauthCallbackUri = new Uri(reader.ReadLine());
+            }
+        }
+
+        public Uri BaseUri => baseUri;
+        public string OAuthId => oauthId;
+        public string OAuthSecret => oauthSecret;
+        public Uri OAuthCallbackUri => oauthCallbackUri;
+
+        public string OAuthToken {
+            get {
+                var vault = new PasswordVault();
+                var token = vault.FindAllByResource("OAuthToken");
+                if(token.Count == 0) {
+                    return null;
+                }
+                return token[0].Password;
+            }
+            set {
+                var vault = new PasswordVault();
+                var tokens = vault.FindAllByResource("OAuthToken");
+                if(tokens.Count != 0) {
+                    foreach(var token in tokens) {
+                        vault.Remove(token);
+                    }
+                }
+                vault.Add(new PasswordCredential("OAuthToken", "IonUser", value));
+            }
         }
 
         public bool UseShellBackButton {
